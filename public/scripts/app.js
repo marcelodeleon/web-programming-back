29e43f0649a48f2713c67ebecd1e0990805dd95f
marcelodeleon <mdeleoncosentino@gmail.com>
2020-10-25 19:56:38 -0300

    chore: #1 Configure lit-html

---
+++ public/scripts/app.js
@@ -0,0 +1,14 @@
+import { render } from 'https://unpkg.com/lit-html?module';
+
+import { isAuthenticated } from './utils/auth.js';
+import authenticatedApp from './authenticatedApp.js';
+import unauthenticatedApp from './unauthenticatedApp.js';
+
+const renderApp = () => {
+  const app = isAuthenticated() ? authenticatedApp : unauthenticatedApp;
+  render(app(), document.body);
+};
+
+window.addEventListener('auth', renderApp);
+
+renderApp();
