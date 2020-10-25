35a439c2137b1dc06339e9a31119082a04443c29
marcelodeleon <mdeleoncosentino@gmail.com>
2020-10-25 19:56:38 -0300

    chore: #1 Configure lit-html

---
+++ public/scripts/unauthenticatedApp.js
@@ -0,0 +1,10 @@
+import { html } from 'https://unpkg.com/lit-html?module';
+
+import loginForm from './templates/loginForm.js';
+
+const unauthenticatedApp = () => html`
+  <h1>Log in to <strong>WPJ</strong></h1>
+  ${loginForm()}
+`;
+
+export default unauthenticatedApp;
