8b7e285acda0de750452a28c91e24b20e0232ea9
nicomach99 <nicomach99@users.noreply.github.com>
2020-10-25 21:47:16 -0300

    Estilos integrados a lo bruto

--- public/scripts/unauthenticatedApp.js
+++ public/scripts/unauthenticatedApp.js
@@ -3,6 +3,7 @@ import { html } from 'https://unpkg.com/lit-html?module';
 import loginForm from './templates/loginForm.js';
 
 const unauthenticatedApp = () => html`
+  <home></home> 
   <h1>Log in to <strong>WPJ</strong></h1>
   ${loginForm()}
 `;
