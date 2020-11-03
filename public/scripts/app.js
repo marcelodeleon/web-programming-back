641146fe01f1e82c6ef27f5f88b6f00fdfcec7c9
nicomach99 <nicomach99@users.noreply.github.com>
2020-11-03 11:39:32 -0300

    Template with welded data

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@ import { isAuthenticated } from './utils/auth.js';
 import authenticatedApp from './authenticatedApp.js';
 import unauthenticatedApp from './unauthenticatedApp.js';
 import home from './templates/home.js';
+import connections from './templates/connections.js'
 
 const renderApp = () => {
   const app = isAuthenticated() ? authenticatedApp : home; //antes de home: unauthenticatedApp
