7c878d4fab5ac20bb9467ae3bf02df04d9363223
nicomach99 <nicomach99@users.noreply.github.com>
2020-11-04 18:28:32 -0300

    Template con estilos rústicos

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,6 +4,7 @@ import { isAuthenticated } from './utils/auth.js';
 import authenticatedApp from './authenticatedApp.js';
 import unauthenticatedApp from './unauthenticatedApp.js';
 import home from './templates/home.js';
+import product from './templates/product.js';
 
 const renderApp = () => {
   const app = isAuthenticated() ? authenticatedApp : home; //antes de home: unauthenticatedApp
