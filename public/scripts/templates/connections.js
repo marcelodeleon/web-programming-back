8dbf0ee3d557c07a11a94e779d408e225d7c3793
nicomach99 <nicomach99@users.noreply.github.com>
2020-11-18 01:45:01 -0300

    update connection template

--- public/scripts/templates/connections.js
+++ public/scripts/templates/connections.js
@@ -1,148 +1,72 @@
 import { html } from 'https://unpkg.com/lit-html?module';
 
 const connections = () => {
+  // Objetc for testing
+  const objProduct = {};
+  objProduct.name = 'Guitarra Criolla Fender!';
+  objProduct.photos = [
+    'https://media.fanaticguitars.com/2016/05/alhambra-4p-1.jpg',
+    'https://upload.wikimedia.org/wikipedia/commons/e/e8/Classical_Guitar_two_views.jpg',
+  ];
+  objProduct.descrption =
+    'Excelente sonido. Afina bien y está en muy buen estado. Solo tiene un detalle que se aprecia en la última foto, pero no afecta ni el sonido ni el funcionamiento de la misma.';
 
-  //Objetc for testing
-  var objProduct = new Object();
-  objProduct.name = "Guitarra Criolla Fender!";
-  objProduct.photos = ["https://media.fanaticguitars.com/2016/05/alhambra-4p-1.jpg","https://upload.wikimedia.org/wikipedia/commons/e/e8/Classical_Guitar_two_views.jpg"]
-  objProduct.descrption = "Excelente sonido. Afina bien y está en muy buen estado. Solo tiene un detalle que se aprecia en la última foto, pero no afecta ni el sonido ni el funcionamiento de la misma.";
+  const objPerson = {};
+  objPerson.name = 'Oscar Lopez';
+  objPerson.phone = '098 654 445';
+  objPerson.email = 'oscarlopez45@gmail.com';
 
-  var objPerson = new Object();
-  objPerson.name = "Oscar Lopez";
-  objPerson.phone = "098 654 445";
-  objPerson.email = "oscarlopez45@gmail.com";
-
-  var myconnections = [[objProduct, objPerson], [objProduct, objPerson]]
-
-  function handlerOffer() {
-    console.log("Realizar Offerta");
-  }
-
+  const myconnections = [
+    [objProduct, objPerson],
+    [objProduct, objPerson],
+  ];
 
   return html`
     <style>
-      .container {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: nowrap;
-        justify-content: center;
-        align-content: flex-start;
-        align-items: stretch;
-        
-      }
-
-      .left_container {
-        order: 0;
-        flex: 0 1 auto;
-        align-self: center;
-      }
-
-      .right_container {
-        order: 0;
-        flex: 0 1 auto;
-        align-self: center;
-      }
-
-      .arrow {
-        height: 40px;
-        width: 40px;
-        font-weight: bold;
-        font-size: medium;
-      }
-
-      .tittle {
-        font-weight: bold;
-        text-align: center;
-      }
-
-      .image {
-        margin: 15px;
-        margin-top: 0px;
-        border-radius: 5%;
-      }
-
-      .description {
-        border-radius: 3%;
-        width: 400px;
-        height: auto;
-        padding: 15px;
-        background-color: #bf9663;
-      }
-
-      .buttons_container {
-        display: flex;
-        flex-direction: row;
-        flex-wrap: nowrap;
-        justify-content: space-between;
-        align-content: center;
-        align-items: stretch;
-      }
-
-      .btn {
-        margin: 15px;
-        margin-left: 40px;
-        margin-right: 40px;
-        height: 50px;
-        width: 130px;
-        font-weight: bold;
-        font-size: medium;
-      }
-
       .connTittle {
-        margin-left: 10px;
+        margin-left: 15px;
       }
 
       .connContainer {
-        padding: 10px;
         text-align: left;
-
-        background-color: rgb(28, 141, 179);
-        height: 800px;
-        border-radius: 10px;
       }
 
       .connProdPhoto {
         border-radius: 10px;
-
         margin: 10px;
-      }
-
-      .connProdTittle {
-        font-weight: bold;
       }
 
       .connection {
-        border-style: inset;
-        margin: 10px;
-        padding: 10px;
+        border-radius: 10px;
+        margin: 15px;
+        padding: 15px;
         height: 260px;
+        background-color: rgb(28, 141, 179);
       }
     </style>
+
     <h1 class="connTittle">Tus conexiones:</h1>
     <div class="connContainer">
+      ${myconnections.map(
+        () =>
+          html` <div class="connection">
+            <p>Ofrecen: <strong>${objProduct.name}</strong></p>
+            <p>Por tu: <strong>${objProduct.name}</strong></p>
 
-    ${myconnections.map(p => 
-      html`
-      <div class="connection">
-      <h2 class="connProdTittle">${objProduct.name}</h2>
+            <img
+              class="connProdPhoto"
+              src="${objProduct.photos[0]}"
+              alt="product photo"
+              width="175"
+              height="175"
+              align="right"
+            />
 
-      <img
-        class="connProdPhoto"
-        src="${objProduct.photos[0]}"
-        alt="product photo"
-        width="175"
-        height="175"
-        align="right"
-      />
-
-      <p class="connProdDescription">${objProduct.descrption}</p>
-
-      <h2 class="connProdTittle">Propietario: ${objPerson.name}</h2>
-      <p>${objPerson.phone}</p>
-      <p>${objPerson.email}</p>
-    </div>
-      `
+            <p class="connProdDescription">" ${objProduct.descrption} "</p>
+            <p>Propietario: <strong>${objPerson.name}</strong></p>
+            <p>Teléfono: <strong>${objPerson.phone}</strong></p>
+            <p>Email: <strong>${objPerson.email}</strong></p>
+          </div>`,
       )}
     </div>
   `;
