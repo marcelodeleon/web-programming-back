b801bf4db331de71c6d96ac501262e257b466502
nicomach99 <nicomach99@users.noreply.github.com>
2020-11-18 21:30:54 -0300

    -

--- public/scripts/templates/home.js
+++ public/scripts/templates/home.js
@@ -1,47 +1,93 @@
 import { html } from 'https://unpkg.com/lit-html?module';
 import refresh from '../utils/refresh.js';
 import { addOffer } from '../services/offers.js';
+import { getUser } from '../services/users.js';
+
+
+
+//DATASET:
+  let objProduct1 = { 
+    name : 'Guitarra Criolla Fender!',
+    photos : [
+      'https://media.fanaticguitars.com/2016/05/alhambra-4p-1.jpg',
+      'https://upload.wikimedia.org/wikipedia/commons/e/e8/Classical_Guitar_two_views.jpg',
+      ],
+    descrption :
+      'Excelente sonido. Afina bien y está en muy buen estado. Solo tiene un detalle que se aprecia en la última foto, pero no afecta ni el sonido ni el funcionamiento de la misma.'  
+  };
+  let objProduct2 = { 
+    name : 'Lampara Veladora Antigua',
+    photos : [
+      'https://http2.mlstatic.com/D_NQ_NP_861758-MLU43231537979_082020-V.jpg',
+      'https://http2.mlstatic.com/D_NQ_NP_494301-MLU20318770834_062015-V.jpg',
+      'https://http2.mlstatic.com/D_NQ_NP_929493-MLU43658590014_102020-V.jpg'],
+    descrption :
+      'Restaurada de casa antigua, ya no la uso más.'  
+  };
+  let objProduct3 = { 
+    name : 'Escritorio para PC c/cajón',
+    photos : [
+      'https://http2.mlstatic.com/escritorio-para-pc-con-un-cajon-usado-D_NQ_NP_623345-MLU43646133204_102020-F.jpg',
+      'https://http2.mlstatic.com/escritorio-para-pc-con-un-cajon-usado-D_NQ_NP_753934-MLU43646126380_102020-F.jpg',
+      'https://http2.mlstatic.com/escritorio-para-pc-con-un-cajon-usado-D_NQ_NP_766701-MLU43646121582_102020-F.jpg'],
+    descrption :
+      'Me compré otro y lo tengo ocupando espacio, escucho propuestas!'  
+  };
+
+  let lisProducts = [objProduct1, objProduct2, objProduct3];
+  let lismyProducts = ['Lote de libros', 'Silla de escritorio', 'Bicicleta GT R20'];
+//END DATASET:
 
 let photosCounter = 0;
 let loading = false;
-
-const myProducts = ['Lote de libros', 'Silla de escritorio', 'Bicicleta R20'];
-
-  // Objetc for testing
-  const objProduct = {};
-  objProduct.name = 'Guitarra Criolla Fender!';
-  objProduct.photos = [
-    'https://media.fanaticguitars.com/2016/05/alhambra-4p-1.jpg',
-    'https://upload.wikimedia.org/wikipedia/commons/e/e8/Classical_Guitar_two_views.jpg',
-  ];
-  objProduct.descrption =
-    'Excelente sonido. Afina bien y está en muy buen estado. Solo tiene un detalle que se aprecia en la última foto, pero no afecta ni el sonido ni el funcionamiento de la misma.';
+let currentProduct = 0;
 
 const home = () => {
+  let objProduct = lisProducts[currentProduct];
+
+  // const fetchUser = async () => {
+  //   //GET USERID
+    
+  //   const { data: user } = await getUser(userId);
+
+  //   if (!user) {
+  //     return html`<p>El producto seleccionado no existe!</p>`;
+  //   }
+
+  //   console.log(user);
+  // };
+
+  // fetchUser();
+
   const handlerDoOffer = async (event) => {
     loading = true;
     refresh();
     event.preventDefault();
 
+     
     
     /* To get the selected product in dropdown */
     let combo = document.getElementById("producto");
     let selected = combo.options[combo.selectedIndex].text;
 
     /* Data to build the offer*/
-    const offeredProductname = selected;
-    const wantedProductname = objProduct.name;
-    const offeredProductDescription = objProduct.descrption;
-    const email = "soldado@gmail.com"; //Se debe cargar el mail del usuario logeado
-    const fullName = "nombre soldado";//Se debe cargar el nombre del usuario logeado
-    const phone = "123 456 789"; //Se debe cargar el nombre del usuario logeado
-    const offeredProductPhotos = [
-      'https://media.fanaticguitars.com/2016/05/alhambra-4p-1.jpg',
-      'https://upload.wikimedia.org/wikipedia/commons/e/e8/Classical_Guitar_two_views.jpg',
-    ];//Se debe cargar las fotos del producto ofertado 
+    //COMPLET WITH THE FETCH INFOMATION----------
+    let objOffer = {
+      offeredProductname : selected,
+      wantedProductname : objProduct.name,
+      offeredProductDescription : objProduct.descrption,
+      offeredProductPhotos : [
+        'https://media.fanaticguitars.com/2016/05/alhambra-4p-1.jpg',
+        'https://upload.wikimedia.org/wikipedia/commons/e/e8/Classical_Guitar_two_views.jpg',
+      ],
+      contactInformation:{
+      email : "soldado@gmail.com",
+      fullName : "nombre soldado",
+      phone : "123 456 789"}  
+    }
 
     try {
-      await addOffer({offeredProductname, wantedProductname, offeredProductDescription, offeredProductPhotos, contactInformation:{email,fullName, phone} });
+      await addOffer(objOffer);
     } catch (error) {
       alert(error.message);
     } finally {
@@ -50,10 +96,11 @@ const home = () => {
     }
   }
 
-  function handlerDiscard() {
-    console.log('Descartar producto, cargar siguiente');
+  function handlerNextProduct() {
+    currentProduct === lisProducts.length -1 ? currentProduct=0 : currentProduct++;
+    photosCounter = 0;
+    refresh();
   }
-
 
   function handlerPreviusPhoto() {
     if (objProduct.photos[photosCounter - 1]) {
@@ -177,9 +224,9 @@ const home = () => {
         </div>
 
         <div class="myProducts_container">
-          <p>Elige cuál producto deseas ofrecer a cambio</p>
+          <p>Elige cuál de tus productos deseas ofrecer a cambio:</p>
           <select name="objProduct" id="producto">
-            ${myProducts.map((p) => html`<option value="p">${p}</option>`)}
+            ${lismyProducts.map((p) => html`<option value="p">${p}</option>`)}
           </select>
           <br />
         </div>
@@ -188,8 +235,8 @@ const home = () => {
           <button class="btn offer" type="button" @click="${handlerDoOffer}">
           ${loading ? 'Loading...' : 'Ofertar'}
           </button>
-          <button class="btn reject" type="button" @click="${handlerDiscard}">
-            Descartar
+          <button class="btn reject" type="button" @click="${handlerNextProduct}">
+          ${loading ? 'Loading...' : 'Siguiente'}
           </button>
         </div>
       </div>
